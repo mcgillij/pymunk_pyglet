a29c880baae446f9cc97abe8a3538b6b9e2d7276
Jason McGillivray <mcgillivray.jason@gmail.com>
2012-10-18 00:23:48 -0300

    cleaned up the code

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import sys, random, math
+#IGNORE:C0301
+import sys, math
 import pyglet
 from pyglet.gl import *
 import pymunk
 from pymunk.vec2d import Vec2d
-
 
 def cpfclamp(f, min_, max_):
     """Clamp f between min and max"""
@@ -17,7 +17,6 @@ pyglet.options['debug_gl'] = False
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 DT = 1./FPS
-
 PLAYER_VELOCITY = 100. *2.
 PLAYER_GROUND_ACCEL_TIME = 0.05
 PLAYER_GROUND_ACCEL = (PLAYER_VELOCITY/PLAYER_GROUND_ACCEL_TIME)
@@ -33,7 +32,7 @@ PLATFORM_SPEED = 1
 
 class Main(pyglet.window.Window):
     is_event_handler = True
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         platform = pyglet.window.get_platform()
         display = platform.get_default_display()
         screen = display.get_default_screen()
@@ -45,7 +44,12 @@ class Main(pyglet.window.Window):
         super(Main, self).__init__(resizable=True, width=WIDTH, height=HEIGHT, caption="pymunk test" , context=context)
         self.push_handlers(self.kb_handler)
         self.space = pymunk.Space()   
-        self.space.gravity = 0,-1000
+        self.space.gravity = 0, -1000
+        self.batch = pyglet.graphics.Batch()
+        self.direction = 1
+        self.remaining_jumps = 2
+        self.landing = {'p':Vec2d.zero(), 'n':0}
+        self.landed_previous = False
         # box walls 
         self.static = [
             pymunk.Segment(self.space.static_body, (10, 50), (300, 50), 5)
@@ -75,7 +79,6 @@ class Main(pyglet.window.Window):
             , pymunk.Segment(self.space.static_body, (50, 250), (200, 250), 5)
             , pymunk.Segment(self.space.static_body, (10, 370), (50, 250), 5)
                     ]
-        
         for s in self.static + self.platforms + self.rounded:
             s.friction = 1.
             s.group = 1
@@ -105,23 +108,17 @@ class Main(pyglet.window.Window):
                 return False
                 
         self.space.add_collision_handler(1,2, begin=passthrough_handler)
-
         # player
         self.body = pymunk.Body(5, pymunk.inf)
         self.body.position = 100,100
-        
-        
         self.head = pymunk.Circle(self.body, 10, (0,5))
         self.head2 = pymunk.Circle(self.body, 10, (0,13))
         self.feet = pymunk.Circle(self.body, 10, (0,-5))
-
         self.head.layers = self.head2.layers = 0b1000
         self.feet.collision_type = 1
         self.feet.ignore_draw = self.head.ignore_draw = self.head2.ignore_draw = True
-        
         self.space.add(self.body, self.head, self.feet, self.head2)
         self.target_vx = 0
-    
 
     def on_key_press(self, k, m):
         if k == pyglet.window.key.ESCAPE:
@@ -136,145 +133,132 @@ class Main(pyglet.window.Window):
                 self.body.velocity.y = self.ground_velocity.y + jump_v;
                 self.remaining_jumps -=1
         
-            
-        
     def on_key_release(self, k, m):
         if k == pyglet.window.key.UP:
             self.body.velocity.y = min(self.body.velocity.y, JUMP_CUTOFF_VELOCITY)
+            
+    def draw_lines(self):
+        self.batch = pyglet.graphics.Batch()
+        for shape in self.space.shapes:
+        #shape.body.is_static and
+            if  isinstance(shape, pymunk.Segment):
+                body = shape.body
+                pv1 = body.position + shape.a.rotated(body.angle)
+                pv2 = body.position + shape.b.rotated(body.angle)
+                pyglet.graphics.draw(2, GL_LINES,
+                                     ('v2f', (pv1.x, pv1.y, pv2.x, pv2.y)),
+                                      ('c4f', (1.0, 1.0, 1.0, 1.) * 2))
+            elif isinstance(shape, pymunk.Poly):
+                ps = shape.get_points()
+                ps = [ps[0]] + ps + [ps[0], ps[0]]
+                xs = []
+                for p in ps:
+                    xs.append(p.x)
+                    xs.append(p.y)
+                self.batch.add(len(ps), GL_LINE_STRIP, None, 
+                        ('v2f', xs),
+                         ('c4f', (0.0, 1.0, 0.0, 1.0) * len(ps)))
+                                      
+            elif isinstance(shape, pymunk.Circle):
+                p = shape.body.position
+                ps = [p + (0,shape.radius), p + (shape.radius,0),
+                        p + (0,-shape.radius), p + (-shape.radius,0)]
+                ps += [ps[0]]
+                xs = []
+                for p in ps:
+                    xs.append(p.x)
+                    xs.append(p.y)
+                self.batch.add(len(ps), GL_LINE_STRIP, None,
+                         ('v2f', xs),
+                          ('c4f', (0.0, 0.0, 1.0, 1.0) * len(ps)))
+
+    def movement(self):
+        grounding = {
+            'normal' : Vec2d.zero(),
+            'penetration' : Vec2d.zero(),
+            'impulse' : Vec2d.zero(),
+            'position' : Vec2d.zero(),
+            'body' : None
+        }
+        # find out if player is standing on ground
+        def f(arbiter):
+            n = -arbiter.contacts[0].normal
+            if n.y > grounding['normal'].y:
+                grounding['normal'] = n
+                grounding['penetration'] = -arbiter.contacts[0].distance
+                grounding['body'] = arbiter.shapes[1].body
+                grounding['impulse'] = arbiter.total_impulse
+                grounding['position'] = arbiter.contacts[0].position
+        self.body.each_arbiter(f)
+        self.well_grounded = False
+        if grounding['body'] != None and abs(grounding['normal'].x/grounding['normal'].y) < self.feet.friction:
+            self.well_grounded = True
+            self.remaining_jumps = 2
+    
+        self.ground_velocity = Vec2d.zero()
+        if self.well_grounded:
+            self.ground_velocity = grounding['body'].velocity
+            
+        self.target_vx = 0
+        if self.body.velocity.x > .01:
+            self.direction = 1
+        elif self.body.velocity.x < -.01:
+            self.direction = -1
+        if self.kb_handler[pyglet.window.key.LEFT]:
+            self.direction = -1
+            self.target_vx -= PLAYER_VELOCITY
+        if self.kb_handler[pyglet.window.key.RIGHT]:
+            self.direction = 1
+            self.target_vx += PLAYER_VELOCITY
+        if self.kb_handler[pyglet.window.key.DOWN]:
+            self.direction = -3    
+        self.feet.surface_velocity = self.target_vx, 0
+
+        if grounding['body'] != None:
+            self.feet.friction = -PLAYER_GROUND_ACCEL/self.space.gravity.y #IGNORE:E1101
+            self.head.friciton = HEAD_FRICTION
+        else:
+            self.feet.friction, self.head.friction = 0, 0
+        # Air control
+        if grounding['body'] == None:
+            self.body.velocity.x = cpflerpconst(self.body.velocity.x, self.target_vx + self.ground_velocity.x, PLAYER_AIR_ACCEL * DT)
+        self.body.velocity.y = max(self.body.velocity.y, -FALL_VELOCITY) # clamp upwards as well?
+        
+        # Move the moving platform
+        destination = self.platform_path[self.platform_path_index]
+        current = Vec2d(self.platform_body.position)
+        distance = current.get_distance(destination)
+        if distance < PLATFORM_SPEED:
+            self.platform_path_index += 1
+            self.platform_path_index = self.platform_path_index % len(self.platform_path)
+            t = 1
+        else:
+            t = PLATFORM_SPEED / distance
+        new = current.interpolate_to(destination, t)
+        self.platform_body.position = new
+        self.platform_body.velocity = (new - current) / DT
+            
+        # Did we land?
+        if abs(grounding['impulse'].y) / self.body.mass > 200 and not self.landed_previous:
+            #sound.play()
+            self.landing = {'p':grounding['position'],'n':5}
+            self.landed_previous = True
+        else:
+            self.landed_previous = False
+        if self.landing['n'] > 0:
+            #pygame.draw.circle(screen, pygame.color.THECOLORS['yellow'], to_pygame(landing['p'], screen), 5)
+            self.landing['n'] -= 1
 
     def main(self):
         fps = pyglet.clock.ClockDisplay(clock=self.clock)
-        self.direction = 1
-        self.remaining_jumps = 2
-        landing = {'p':Vec2d.zero(), 'n':0}
         frame_number = 0
-        
-        landed_previous = False
         while True:
             self.dispatch_events()
-            grounding = {
-                'normal' : Vec2d.zero(),
-                'penetration' : Vec2d.zero(),
-                'impulse' : Vec2d.zero(),
-                'position' : Vec2d.zero(),
-                'body' : None
-            }
-            # find out if player is standing on ground
-            def f(arbiter):
-                n = -arbiter.contacts[0].normal
-                if n.y > grounding['normal'].y:
-                    grounding['normal'] = n
-                    grounding['penetration'] = -arbiter.contacts[0].distance
-                    grounding['body'] = arbiter.shapes[1].body
-                    grounding['impulse'] = arbiter.total_impulse
-                    grounding['position'] = arbiter.contacts[0].position
-            self.body.each_arbiter(f)
-                
-            self.well_grounded = False
-            if grounding['body'] != None and abs(grounding['normal'].x/grounding['normal'].y) < self.feet.friction:
-                self.well_grounded = True
-                self.remaining_jumps = 2
-        
-            self.ground_velocity = Vec2d.zero()
-            if self.well_grounded:
-                self.ground_velocity = grounding['body'].velocity
-                
-            self.target_vx = 0
-            
-            if self.body.velocity.x > .01:
-                self.direction = 1
-            elif self.body.velocity.x < -.01:
-                self.direction = -1
-       
-            if self.kb_handler[pyglet.window.key.LEFT]:
-                self.direction = -1
-                self.target_vx -= PLAYER_VELOCITY
-            if self.kb_handler[pyglet.window.key.RIGHT]:
-                self.direction = 1
-                self.target_vx += PLAYER_VELOCITY
-            if self.kb_handler[pyglet.window.key.DOWN]:
-                self.direction = -3    
-
-                
-            
-            self.feet.surface_velocity = self.target_vx,0
-
-        
-            if grounding['body'] != None:
-                self.feet.friction = -PLAYER_GROUND_ACCEL/self.space.gravity.y #IGNORE:E1101
-                self.head.friciton = HEAD_FRICTION
-            else:
-                self.feet.friction, self.head.friction = 0,0
-            
-            # Air control
-            if grounding['body'] == None:
-                self.body.velocity.x = cpflerpconst(self.body.velocity.x, self.target_vx + self.ground_velocity.x, PLAYER_AIR_ACCEL * DT)
-            
-            self.body.velocity.y = max(self.body.velocity.y, -FALL_VELOCITY) # clamp upwards as well?
-            
-            # Move the moving platform
-            destination = self.platform_path[self.platform_path_index]
-            current = Vec2d(self.platform_body.position)
-            distance = current.get_distance(destination)
-            if distance < PLATFORM_SPEED:
-                self.platform_path_index += 1
-                self.platform_path_index = self.platform_path_index % len(self.platform_path)
-                t = 1
-            else:
-                t = PLATFORM_SPEED / distance
-            new = current.interpolate_to(destination, t)
-            self.platform_body.position = new
-            self.platform_body.velocity = (new - current) / DT
-                
-            # Did we land?
-            if abs(grounding['impulse'].y) / self.body.mass > 200 and not landed_previous:
-                #sound.play()
-                landing = {'p':grounding['position'],'n':5}
-                landed_previous = True
-            else:
-                landed_previous = False
-            if landing['n'] > 0:
-                #pygame.draw.circle(screen, pygame.color.THECOLORS['yellow'], to_pygame(landing['p'], screen), 5)
-                landing['n'] -= 1            
-            # Drawing    
+            self.movement()
+            # Drawing
             self.clear()
-            
-            for shape in self.space.shapes:
-            #shape.body.is_static and
-                if  isinstance(shape, pymunk.Segment):
-                    body = shape.body
-                    pv1 = body.position + shape.a.rotated(body.angle)
-                    pv2 = body.position + shape.b.rotated(body.angle)
-                    pyglet.graphics.draw(2, GL_LINES,
-                                         ('v2f', (pv1.x, pv1.y, pv2.x, pv2.y)),
-                                          ('c4f', (1.0, 1.0, 1.0, 1.) * 2))
-            
-            b = pyglet.graphics.Batch()
-            for shape in self.space.shapes:
-                if isinstance(shape, pymunk.Poly):
-                    ps = shape.get_points()
-                    ps = [ps[0]] + ps + [ps[0], ps[0]]
-                    xs = []
-                    for p in ps:
-                        xs.append(p.x)
-                        xs.append(p.y)
-                    b.add(len(ps), GL_LINE_STRIP, None, 
-                            ('v2f', xs),
-                             ('c4f', (0.0, 1.0, 0.0, 1.0) * len(ps)))
-                                          
-                if isinstance(shape, pymunk.Circle):
-                    p = shape.body.position
-                    ps = [p + (0,shape.radius), p + (shape.radius,0),
-                            p + (0,-shape.radius), p + (-shape.radius,0)]
-                    ps += [ps[0]]
-                    xs = []
-                    for p in ps:
-                        xs.append(p.x)
-                        xs.append(p.y)
-                    pyglet.graphics.draw(len(ps), GL_LINE_STRIP,
-                                         ('v2f', xs),
-                                          ('c4f', (0.0, 0.0, 1.0, 1.0) * len(ps)))
-            b.draw()    
+            self.draw_lines()
+            self.batch.draw()    
             fps.draw()
             self.space.step(1/50.0)
             self.clock.tick(50)
